0ea3c06109b89907bfa886030fe1caf66d2c46cd
Pratik-Shrivastava <pratikkumar441@gmail.com>
2024-02-05 23:42:03 +0530

    Convert the displayed number to string

--- myScript.js
+++ myScript.js
@@ -1,9 +1,17 @@
 const valueBox = document.getElementById('number');
-let number = valueBox.innerText;
+let stack = new Stack();
+let number = String(valueBox.innerText);
 const add = (object) =>{
     console.log(`${object}`)
-    number = number*10;
-    number = number + (Number(object.id));
+    if(number === "0")
+    {
+        number = object.id;        
+    }
+    else
+    {
+        number = number.concat(object.id);
+    }
+        
     console.log(`${number}`);
     changeValue(number);
 }
@@ -12,18 +20,47 @@ const changeValue = (number) => {
 }
 const clearAll = () =>
 {
-    number = 0;
+    number = "0";
     console.log(number);
     changeValue(number);
 }
 
 const remove = () =>{
-    number = number / 10;
-    number = ~~number;
+    if(number.length === 1)
+    {
+        number = "0";
+    }
+    else
+    {
+        number = number.slice(0,-1);
+    }
     changeValue(number);
 }
 const setToZero = () => {
     number = 0;
+}
+const addSign = (object) => {
+    console.log(`${object.id}`);
+    let sign = String(object.id);
+    switch (sign) {        
+        case "add":
+            number=number.concat("+");            
+            console.log(`${number}`);
+            break;
+        case "minus":
+            number=number.concat("-");
+            break;
+        case "multiply":
+            number=number.concat("x");
+            break;
+        case "divide":
+            number=number.concat("/");
+            break;
+        case "dot":
+            number=number.concat(".");
+            break;
+    }
+    changeValue(number);
 }
 
 // console.log(Number(number));
